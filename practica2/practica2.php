278ff05a61fbb50920101d812070fea3e42be443
velazqueztessa <94088547+velazqueztessa@users.noreply.github.com>
2021-11-10 22:45:53 -0700

    Add files via upload

--- practica2/practica2.php
+++ practica2/practica2.php
@@ -1,30 +1,38 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>Practica 2</title>
+	<meta charset="utf-8">
+	<title>Practica 2. PHP dentro de HTML</title>
 </head>
 <body>
+       <h1>Nombre: Velazquez Rodriguez Tessa Airam 5AVP</h1>
+    <?php 
+       $numero = rand(1,4);
+       $color = ["lightblue", "lightpink", "darkorchid", "gray" ];
+       $numcolor = rand(0,3);
 
-<?php
-$numero = rand(1,4);
-$color = ["#ffc1e1", "#bdfcff","#e1bdff","#c49361"];
-$numcolor = rand(0,3);
+       echo "numero generado = " .$numero."<br>";
+       echo "<div style = 'color:".$color[$numcolor]. "'>Color generado</div>";
 
-echo "numero generado = ".$numero;
-echo "<div style='color:".$color[$numcolor]."'>Color generado</div>";
+       if ($numero == 1) {
+       
+     ?>
+           <h1 style="color: <?php echo $color[$numcolor] ?>">Titulo de la pagina 1</h1>
 
-if($numero == 1){
-?>
-<h1 style="color: <?php echo $color[$numcolor]; ?>">Titulo de la pagina 1</h1>
+    <?php } elseif($numero == 2) { ?> 
 
-<?php } elseif ($numero == 2) { ?>
-<h2 style="color: <?php echo $color[$numcolor]; ?>">Titulo de la pagina 2</h1>
+           <h1 style="color: <?php echo $color[$numcolor] ?>">Titulo de la pagina 2</h1>
 
-<?php } elseif ($numero == 3) { ?>
-<h2 style="color: <?php echo $color[$numcolor]; ?>">Titulo de la pagina 3</h1>
+    <?php }elseif ($numero == 3) {  ?>
 
-<?php } elseif ($numero == 4) { ?>
-<h2 style="color: <?php echo $color[$numcolor]; ?>">Titulo de la pagina 4</h1>
+           <h1 style="color: <?php echo $color[$numcolor] ?>">Titulo de la pagina 3</h1>
 
-<?php } ?>
+   <?php }else{  ?>
+
+           <h1 style="color: <?php echo $color[$numcolor] ?>">Titulo de la pagina 4</h1>
+
+ <?php }  ?>
+
+
 </body>
 </html>
